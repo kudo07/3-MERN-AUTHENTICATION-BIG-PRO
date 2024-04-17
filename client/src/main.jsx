4bc23d4590ff5ef3a4808b91aebf721337e36f0e
kudo07 <134780035+kudo07@users.noreply.github.com>
2024-04-18 04:09:25 +0530

    completed redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,18 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
 import { Provider } from 'react-redux';
 import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate persistor={persistor} loading={null}>
+      {/* loading is give in the site */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice.js';
+import storage from 'redux-persist/lib/storage';
+// save the data inside the browser
 
+import { persistReducer, persistStore } from 'redux-persist';
+
+//
+const rootReducer = combineReducers({ user: userReducer });
+//
+const persistConfig = {
+  key: 'root',
+  // "root can be replaced any name"
+  version: 1,
+  // defualt -1 if we dont defined
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+//
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+export const persistor = persistStore(store);
+// save inside the local storage
