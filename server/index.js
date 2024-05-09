b1ba274dd304a8b3142386161bcb64e1d1f30ed0
kudo07 <134780035+kudo07@users.noreply.github.com>
2024-05-09 13:48:35 +0530

    changes deployment

--- server/index.js
+++ server/index.js
@@ -5,14 +5,31 @@ import connectMongoDB from './db/connectMongoDB.js';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 dotenv.config();
+import path from 'path';
 
+//
+const __dirname = path.resolve();
+// dynamic directory name
 // call
 const app = express();
+// define the static folder for the build in production
+// create our static path
+app.use(express.static(path.join(__dirname, '/client/dist')));
+// find this directory and send it to this file
+// in vite dist
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+// any place or any thing inside the client side
+
+// any
+//
 const PORT = process.env.PORT || 3000;
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 // example
 app.get('/', (req, res) => {
   res.send('server is ready and running backend in index.js');
