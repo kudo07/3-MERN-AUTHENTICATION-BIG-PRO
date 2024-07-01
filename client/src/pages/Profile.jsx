880d6f019acb965cd999362b6875bd742ba378c6
unknown <vknwl077@gmail.com>
2024-07-02 00:28:18 +0530

    package.json

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -134,7 +134,7 @@ const Profile = () => {
         />
 
         <img
-          src={currentUser.profilePicture || formData.profilePicture}
+          src={formData.profilePicture || currentUser.profilePicture}
           alt="profile"
           className="h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2"
           onClick={() => fileRef.current.click()}
